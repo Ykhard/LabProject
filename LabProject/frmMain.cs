d62c05f5b6a6efebcd30990ea9f0d6d2a7336487
Ykhard <yahya.2007@hotmail.com>
2020-04-18 06:36:39 +0300

    1848012

---
+++ LabProject/frmMain.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace IE322_App_KAU
+{
+    public partial class frmMain : Form
+    {
+        string username = "Yahya";
+        string myPassword = "1234";
+        bool loggedIn = false;
+        
+        int attempt = 1;
+        int MaxAttempts = 3;
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRadio_Click(object sender, EventArgs e)
+        {
+            frmRadio frm = new frmRadio();
+            frm.ShowDialog();
+
+        }
+
+        private void BtnEXIT_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCombo_Click(object sender, EventArgs e)
+        {
+            frmCombo frm = new frmCombo();
+            frm.ShowDialog();
+            //to close the form
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void txtUser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPic_Click(object sender, EventArgs e)
+        {
+            frmPic01 frm = new frmPic01();
+            frm.ShowDialog();
+            //to close the form
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void btnPic2_Click(object sender, EventArgs e)
+        {
+            frmPic02 frm = new frmPic02();
+            frm.ShowDialog();
+            //to close the form
+            this.DialogResult = DialogResult.OK;
+        }
+
+        
+
+      
+        
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            if (! loggedIn)
+            {
+
+                while (attempt <= MaxAttempts)
+                {
+                    if (txtUser.Text != username)
+                    {
+                        // username is incorrect
+                        MessageBox.Show("Invalid username, " + (MaxAttempts - attempt) + " attempts remaining");
+                        attempt++;
+                        return;
+                    }
+                    else
+                    {   // username is correct
+                        // so check password			
+                        if (txtPassword.Text != myPassword)
+                        {
+                            // Incorrect password
+                            attempt++;
+                            MessageBox.Show("Incorrect password," + (MaxAttempts - attempt) + " attempts remaining");
+                            return;
+                        }
+                        else
+                        {
+                            //Both are correct
+                            attempt = 1; // reset the number of attempts
+                            loggedIn = true;
+                           // MessageBox.Show("Hi " + username + ", your login successful", "Welcome!!");                          
+                           
+
+                            foreach (var grp in groupBox1.Controls.OfType<GroupBox>())
+                            {
+                                foreach (var btn in grp.Controls.OfType<Button>())
+                                {
+                                    btn.Enabled = true;
+                                    btn.ForeColor = Color.Black;
+                                }
+                            }
+
+                            btnLogin.Text = "Logout";
+                            toolStripStatusLabel1.Text = "Hi "+ username+ ", you are logged in.";
+                            // this.Width = 1600;
+                            break; // come out of while loop
+                        }//endif
+
+                    }//endif
+                }//end while
+            }
+            else 
+            {
+                btnLogin.Text = "Login";
+                toolStripStatusLabel1.Text = "You are logged out.";
+                loggedIn = false;
+
+                txtUser.Clear();
+                txtPassword.Clear();
+
+                foreach (var grp in groupBox1.Controls.OfType<GroupBox>())
+                {
+                    foreach (var btn in grp.Controls.OfType<Button>())
+                    {
+                        btn.Enabled = false;
+                    }
+                }
+            }
+           
+
+
+
+        } //end login Button
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            frmRandom frm = new frmRandom();
+            frm.ShowDialog();
+            //to close the form
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            frmCheck frm = new frmCheck();
+            frm.ShowDialog();
+            //to close the form
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void BtnRoboticCell_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        
+        private void BtnRandomCombo_Click(object sender, EventArgs e)
+        {
+            frmRandomCombo frm = new frmRandomCombo();
+            frm.ShowDialog();
+        }
+
+      
+
+        private void txtUser_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUser_Enter(object sender, EventArgs e)
+        {
+            this.txtUser.BackColor = Color.FromName("yellow");
+        }
+
+        private void txtUser_Leave(object sender, EventArgs e)
+        {
+            this.txtUser.BackColor = Color.FromName("white");
+        }
+
+
+        private void txtPW_Enter(object sender, EventArgs e)
+        {
+            this.txtPassword.BackColor = Color.FromName("yellow");
+        }
+
+        private void txtPW_Leave(object sender, EventArgs e)
+        {
+            this.txtPassword.BackColor = Color.FromName("white");
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        
+      
+
+        private void BtnIteration_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void BtnMethods_Click(object sender, EventArgs e)
+        {
+            frmMethods frm = new frmMethods();
+            frm.ShowDialog();
+        }
+
+      
+        private void BtnArray_Click(object sender, EventArgs e)
+        {
+            frmArray frm = new frmArray();
+            frm.Show();
+        }
+
+        private void btnGroupieApp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
